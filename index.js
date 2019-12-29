b76faff347d7c383aa3db60569cf3767402e88f2
Vlad Rosu <vrosu@vlad.local>
2019-12-29 11:32:34 -0500

    update website

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+var path = require('path')
+var nodemailer = require('nodemailer');
 
-app.use(express.static('.'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 router.get('/about',function(req,res){
   res.sendFile(__dirname+'/about.html');
@@ -10,6 +14,10 @@ router.get('/about',function(req,res){
 
 router.get('/qualifications',function(req,res){
   res.sendFile(__dirname+'/qualifications.html');
+});
+
+router.get('/testimonials',function(req,res){
+  res.sendFile(__dirname+'/testimonials.html');
 });
 
 router.get('/contact',function(req,res){
@@ -23,4 +31,29 @@ router.get('*',function(req,res){
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
+
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
